65edc8985f08301a56844e9a043435ead296db47
Ilango Rajagopal <ilangokul@gmail.com>
2020-02-26 18:27:54 +0530

    Half cosine schedule

--- schedule.py
+++ schedule.py
@@ -1,20 +1,35 @@
-
-schedule = {
-    200: 0.1,
-    400: 0.05,
-    450: 0.01,
-    500: 0.001
-}
+import tensorflow as tf
+import math
 
 
-def lr_schedule(e, lr):
-    global schedule
+def step_lr_schedule(epoch, lr):
+    schedule = {
+        200: 0.1,
+        400: 0.05,
+        450: 0.01,
+        500: 0.001
+    }
     for se, slr in schedule.items():
-        if e < se:
+        if epoch < se:
             return slr
-    if e % 200 == 0:
+    if epoch % 200 == 0:
         return lr/3
     return lr
+
+
+def cos_lr_schedule(epoch, lr):
+    # Half cosine
+    cycles = 490
+    max_lr = 0.1
+    min_lr = 0.001
+    if epoch <= cycles:
+        cosval = (math.cos((math.pi*epoch)/cycles) + 1)/2
+        return cosval * (max_lr-min_lr) + min_lr
+    return min_lr
+
+
+def lr_schedule(epoch, lr):
+    return cos_lr_schedule(epoch, lr)
 
 
 if __name__ == "__main__":
@@ -26,5 +41,5 @@ if __name__ == "__main__":
 
     import matplotlib.pyplot as plt
     plt.plot(lrs)
-    plt.yscale("log")
+    # plt.yscale("log")
     plt.show()
