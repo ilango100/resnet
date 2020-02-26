b85ee6efd5c7898eba65247da22e440cfbbd60d3
Ilango Rajagopal <ilangokul@gmail.com>
2020-02-26 18:27:14 +0530

    Added cats_vs_dogs and imagenette datasets

--- data.py
+++ data.py
@@ -1,7 +1,4 @@
 import math
-from os import sep
-from os.path import join
-import pandas as pd
 import tensorflow as tf
 import tensorflow_datasets as tfds
 
@@ -14,9 +11,9 @@ def split_trainval(dataset, l, frac=0.8):
     return train, val, trsteps
 
 
-def cifar10(datapath, bsize):
+def cifar10(bsize):
     # Download and prepare dataset
-    cifar = tfds.builder("cifar10:3.*.*", data_dir=datapath)
+    cifar = tfds.builder("cifar10")
     cifar.download_and_prepare()
 
     # Get epoch steps
@@ -28,7 +25,8 @@ def cifar10(datapath, bsize):
     # Get data
     # train, val = tfds.Split.TRAIN.subsplit(weighted=[4, 1])
     train, val = "train[:80%]", "train[80%:]"
-    train, val, test = [cifar.as_dataset(split=x, as_supervised=True) for x in [train, val, tfds.Split.TEST]]
+    train, val, test = [cifar.as_dataset(split=x, as_supervised=True) for x in [
+        train, val, tfds.Split.TEST]]
 
     def augment(x, y):
         x = tf.pad(x, [[4, 4], [4, 4], [0, 0]])
@@ -50,9 +48,49 @@ def cifar10(datapath, bsize):
     return train, val, test, trsteps, valsteps, testeps, 10
 
 
-def tiny_imagenet(datapath, bsize):
+def cats_vs_dogs(bsize):
+    cvd = tfds.builder("cats_vs_dogs")
+    cvd.download_and_prepare()
+
+    totsteps = cvd.info.splits[tfds.Split.TRAIN].num_examples
+    trsteps = math.ceil(totsteps*0.7)
+    valsteps = math.ceil(totsteps*0.15)
+    testeps = totsteps - trsteps - valsteps
+
+    train, val, test = "train[:70%]", "train[70%:85%]", "train[85%:]"
+    train, val, test = [cvd.as_dataset(split=x, batch_size=bsize,
+                                       shuffle_files=True, as_supervised=True) for x in [train, val, test]]
+
+    # Per epoch steps
+    trsteps = math.ceil(trsteps/bsize)
+    valsteps = math.ceil(valsteps/bsize)
+    testeps = math.ceil(testeps/bsize)
+
+    return train, val, test, trsteps, valsteps, testeps, 2
+
+
+def imagenette(bsize):
+    imt = tfds.builder("imagenette/160px")
+    imt.download_and_prepare()
+
+    totsteps = imt.info.splits[tfds.Split.TRAIN].num_examples
+    trsteps = math.ceil(totsteps * 0.8)
+    valsteps = totsteps - trsteps
+    testeps = imt.info.splits[tfds.Split.VALIDATION].num_examples
+
+    train, val, test = "train[:80%]", "train[80%:]", "validation"
+    train, val, test = [imt.as_dataset(split=x, batch_size=bsize,
+                                       shuffle_files=True, as_supervised=True) for x in [train, val, test]]
+
+    trsteps, valsteps, testeps = [
+        math.ceil(x/bsize) for x in [trsteps, valsteps, testeps]]
+
+    return train, val, test, trsteps, valsteps, testeps, 10
+
+
+def tiny_imagenet(bsize):
     # Download and prepare dataset
-    cifar = tfds.builder("tiny_imagenet", data_dir=datapath)
+    cifar = tfds.builder("tiny_imagenet")
     cifar.download_and_prepare()
 
     # Get epoch steps
@@ -79,4 +117,4 @@ def tiny_imagenet(datapath, bsize):
     return train, val, test, trsteps, valsteps, testeps, 200
 
 
-__all__ = ["cifar10", "tiny_imagenet"]
+__all__ = ["cifar10", "cats_vs_dogs", "imagenette", "tiny_imagenet"]
